3fcdf21ee8d516cdb3b2be46bebbd36bae771075
eli-bigman <nutsugah81@gmail.com>
2024-10-03 22:22:46 +0000

    add quote around import image

--- src/components/component/searchStory.jsx
+++ src/components/component/searchStory.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import Image from next/image;
+import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Web3 } from "web3";
